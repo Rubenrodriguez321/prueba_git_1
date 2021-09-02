50770754b16598dd1171e9784b1695c996404a2e
ruben <rubo.321@gmail.com>
2021-09-01 20:01:14 -0500

    practica de hola mundo

---
+++ hola_mundo.js
@@ -0,0 +1,6 @@
+const funciones = require('./funciones.js');
+
+console.log (funciones.suma(2,1));
+console.log (funciones.resta(2,1));
+console.log (funciones.multi(2,1));
+console.log (funciones.divi(2,1));
